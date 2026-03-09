49d3d250153ef164f003e5b9bef263152818fd55
opvlad <opvladq@gmail.com>
2026-03-09 12:33:39 +0200

    change lab2

--- lab2/example_project/main.py
+++ lab2/example_project/main.py
@@ -4,3 +4,9 @@ from my_counter_lib.timers import timeout_counter
 counter = counter_generator(1)
 
 timer = timeout_counter(1, counter)
+
+
+def my_test_function(
+    first_long_argument, second_long_argument, third_long_argument, fourth_long_argument
+):
+    pass
